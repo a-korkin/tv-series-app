c774e4921bc63229e0c769b74f52332b476e68aa
a-korkin <a-korkin88@yandex.ru>
2021-10-20 14:38:20 +0300

    hooks added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,18 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
 import "./App.scss";
 
 const App: FC = () => {
+    const { loading, error, data } = useTypedSelector(state => state);
+    const { fetchSeries } = useActions();
+
+    useEffect(() => {
+        fetchSeries();
+    }, []);
+
+    console.log(data);
+
     return (
         <h1>App</h1>
     );
